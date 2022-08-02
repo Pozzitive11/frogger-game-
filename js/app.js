9622acbaa3d38882c13263e7c9fe1ee45f2cc9f8
Pozzitive11 <palaga2442@gmail.com>
2022-08-03 00:04:21 +0300

    create frogger game

--- js/app.js
+++ js/app.js
@@ -1,46 +1,116 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+const playerStart = {
+  x: 210,
+  y: 390,
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+const charactersSize = {
+  charboy: 50,
+  bug: 35,
+  sizeDiff: 12,
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+const tile = {
+  width: 101,
+  height: 85,
+};
+
+const board = {
+  top: 0,
+  right: tile.width * 5,
+  bottom: tile.height * 6,
+  left: 0,
+};
+class Enemy {
+  constructor(y, speed) {
+    this.sprite = "images/enemy-bug.png";
+    this.x = 0;
+    this.y = y;
+    this.speed = speed;
+  }
+
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+  collision() {
+    if (this.x + tile.width > player.x && this.x + tile.width < player.x + tile.width && this.y > player.y && this.y < player.y + tile.height ) {
+      setTimeout(() => {
+        player.startPosition();
+      }, 100);
+    }
+  }
 
+  update(dt) {
+    this.x += this.speed * dt;
+    if (this.x > board.right) this.x = -tile.width;
+    this.collision();
+  }
+}
+class Player {
+  constructor() {
+    this.startPosition();
+    this.sprite = "images/char-boy.png";
+  }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+  startPosition() {
+    this.x = tile.width * 2;
+    this.y = tile.height * 5 - charactersSize.charboy;
+  }
 
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
 
+  reset() {
+    if (this.y < board.top) {
+      this.startPosition();
+    }
+  }
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+  handleInput(key) {
+    switch (key) {
+      case "left":
+        if (this.x > board.left) {
+          this.x -= tile.width;
+        }
+        break;
+      case "right":
+        if (this.x < board.right - tile.width) {
+          this.x += tile.width;
+        }
+        break;
+      case "up":
+        if (this.y > board.top) {
+          this.y -= tile.height;
+        }
+        break;
+      case "down":
+        if (this.y < board.bottom - tile.height * 2) {
+          this.y += tile.height;
+        }
+        break;
+    }
+    this.reset();
+  }
 
-    player.handleInput(allowedKeys[e.keyCode]);
+  update() {}
+}
+
+const allEnemies = [
+  new Enemy(tile.height * 3 - charactersSize.bug, 90),
+  new Enemy(tile.height * 2 - charactersSize.bug, 150),
+  new Enemy(tile.height * 1 - charactersSize.bug, 100),
+];
+
+const player = new Player();
+
+document.addEventListener("keyup", function (e) {
+  const allowedKeys = {
+    37: "left",
+    38: "up",
+    39: "right",
+    40: "down",
+  };
+
+  player.handleInput(allowedKeys[e.keyCode]);
 });
